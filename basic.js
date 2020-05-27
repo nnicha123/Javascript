a964acc28bfb9db479322afa3d076c9f1a777a41
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-05-27 11:35:23 +0700

    added javascript basic file

--- basic.js
+++ basic.js
@@ -1,1 +1,12 @@
 console.log("From external script")
+
+let value1 = "10";
+        let value2 = "2";
+        let add = value1 + value2;
+        let multiply = value1 * value2;
+        let subtract = value1 - value2;
+        let divide = value2 / value2;
+        console.log(add);
+        console.log(multiply);
+        console.log(subtract);
+        console.log(divide);
