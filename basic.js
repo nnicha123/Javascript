c959f43e6ce73aad87cce929d13b2ed48b290182
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-05-28 12:30:16 +0700

    commited javascript exercises

--- basic.js
+++ basic.js
@@ -1,12 +1,30 @@
-console.log("From external script")
+// console.log("From external script")
 
-let value1 = "10";
-        let value2 = "2";
-        let add = value1 + value2;
-        let multiply = value1 * value2;
-        let subtract = value1 - value2;
-        let divide = value2 / value2;
-        console.log(add);
-        console.log(multiply);
-        console.log(subtract);
-        console.log(divide);
+// let value1 = "10";
+//         let value2 = "2";
+//         let add = value1 + value2;
+//         let multiply = value1 * value2;
+//         let subtract = value1 - value2;
+//         let divide = value2 / value2;
+//         console.log(add);
+//         console.log(multiply);
+//         console.log(subtract);
+//         console.log(divide);
+
+    // console.log(Number(undefined));
+    // console.log(Number(null));
+    // console.log(Number(true));
+    // console.log(Number(false));
+    // console.log(Number(""));
+    // console.log(Number(" 154 "));
+    // console.log(Number(" 1 5 4 "));
+    // console.log(Number("240z"));
+    // console.log(Number("240.24"));
+    
+    // let a =1, b=1;
+    // let c = ++a;
+    // let d = b++;
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+    // console.log(d);
---
+++ basic2.js
@@ -0,0 +1,4 @@
+
+        for(let i=2;i<=10;i++){
+            console.log(i);
+        }
