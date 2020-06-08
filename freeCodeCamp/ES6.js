53ef7b710ea0f75e4dde75a60ff0610f2eef30bc
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-08 10:07:44 +0700

    added javascript exercise file

---
+++ freeCodeCamp/ES6.js
@@ -0,0 +1,21 @@
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    "use strict";
+  
+    // Only change code below this line
+    const resultDisplayArray = [
+        `<li class="text-warning">no-var</li>`,
+        `<li class="text-warning">var-on-top</li>`,
+        `<li class="text-warning">linebreak</li>`
+  ];
+    // Only change code above this line
+  
+    return resultDisplayArray;
+  }
+  
+  const resultDisplayArray = makeList(result.failure);
+  console.log(resultDisplayArray);
