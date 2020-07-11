c6985c92c3082fe031d4ed90d8f166f8a80918d6
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-11 08:34:45 +0700

    tried new code

--- try.js
+++ try.js
@@ -1,5 +1,6 @@
-let date = '0910'
-let month = date.slice(0,2)
-let day = date.slice(2)
+let date = '20001102'
+let newd = date.slice(4)
+let month = newd.slice(0,2)
+let day = newd.slice(2)
 let newDate = month + '/' + day
 console.log(newDate)
